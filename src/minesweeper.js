e48f6fd7485f42b24f5d7f299e2780997fdc3323
Blaine Anderson <andersonblaine90@gmail.com>
2018-02-07 22:23:08 -0500

    Created Blank Board With Array

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,13 +1,7 @@
-const blankLine = ' | | ';
-const guessLine = '1| | ';
-const bombLine = ' |B| ';
+const board = [
+  [' ',' ',' '],
+  [' ',' ',' '],
+  [' ',' ',' ']
+];
 
-console.log('This is what an empty board looks like:');
-console.log(blankLine);
-console.log(blankLine);
-console.log(blankLine);
-
-console.log('This is what a board with a guess and a bomb would look like:');
-console.log(guessLine);
-console.log(bombLine);
-console.log(blankLine);
+console.log(board);
